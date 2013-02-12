58fc0e569f99bfc82dc3161dffa2c7be87ec48b5
koyachi <rtk2106@gmail.com>
2013-02-12 23:47:24 +0900

    first commit

---
+++ src/org/buffr/boomkat/MainActivity.java
@@ -0,0 +1,34 @@
+package org.buffr.boomkat;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+import org.buffr.boomkat.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button)findViewById(R.id.search_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, SearchResultsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
---
+++ src/org/buffr/boomkat/SearchResultsActivity.java
@@ -0,0 +1,22 @@
+package org.buffr.boomkat;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SearchResultsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+}
