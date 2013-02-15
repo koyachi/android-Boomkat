3c69bf011d371bf2b1074ac9f1966eb91a6b9a3b
koyachi <rtk2106@gmail.com>
2013-02-15 21:06:43 +0900

    add INTERNET permission to AndroidManifest.xml

--- src/org/buffr/boomkat/SearchResultsActivity.java
+++ src/org/buffr/boomkat/SearchResultsActivity.java
@@ -2,11 +2,14 @@ package org.buffr.boomkat;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class SearchResultsActivity extends Activity {
+    private String TAG = SearchResultsActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +18,13 @@ public class SearchResultsActivity extends Activity {
 
         // should start before come to this activity... => Service?
         Command command = new Command(this, getAssets());
+
+        try {
+            Runtime.getRuntime().exec("chmod 0755 " + getCacheDir().getAbsolutePath());
+        } catch (IOException e) {
+            Log.d(TAG, "failed to chmod cache dir");
+        }
+
         ArrayList<String> args = new ArrayList<String>();
         args.add("search");
         args.add("'Tim Hecker'");
